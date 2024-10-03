aa22fdc6bd106ae9851ad3930bea16d3c1770944
i-Miranda <ivanmirandaart@yahoo.com>
2024-10-03 17:26:41 +0200

    first commit

---
+++ FdP/CAA1-20241/convert.c
@@ -0,0 +1,86 @@
+/*
+ * Author: Ivan Miranda Moral
+ * Date: 03-10-2024
+ * Description: This program takes a Fahrenheit temperature value input
+ * and converts and outputs it to a Kelvin temperature value.
+ */
+
+
+
+
+/* -------------------------------*/
+
+/* System header files */
+#include <stdio.h>
+
+
+
+
+
+
+/* -------------------------------*/ 
+
+/* Symbolic constants */
+
+#define F_FREEZE_POINT 32
+#define FIVE_NINTHS 5/9
+#define K_FREEZE_POINT 273.15
+
+
+/* -------------------------------*/
+
+/* Predeclaration of actions and functions */
+float requestInput(void);
+float convertFToC(float fahrenheit);
+float convertCToK(float celsius);
+float convertFToK(float fahrenheit);
+void printK(float kelvin);
+
+/* -------------------------------*/
+
+/* Main function */
+int main(int argc, char **argv){
+	float fahrenheit;
+	float kelvin;
+
+	fahrenheit = requestInput();
+	kelvin = convertFToK(fahrenheit);
+	printK(kelvin);
+	return 0;
+}
+
+/* -------------------------------*/ 
+
+/* Implementation of actions and functions */
+
+float requestInput(void){
+	float fahrenheit;
+	printf("INPUT\nFAHRENHEIT DEGREES?\n");
+	scanf("%2f", &fahrenheit);
+	return fahrenheit;
+}
+
+float convertFToC(float fahrenheit){
+	float celsius;
+
+	celsius = (fahrenheit - F_FREEZE_POINT) * FIVE_NINTHS;
+	return celsius;
+}
+
+float convertCToK(float celsius){
+	float kelvin;
+
+	kelvin = celsius + K_FREEZE_POINT;
+	return kelvin;
+}
+
+float convertFToK(float fahrenheit){
+	float kelvin;
+
+	kelvin = (fahrenheit - F_FREEZE_POINT) * FIVE_NINTHS + K_FREEZE_POINT;
+	return kelvin;
+}
+
+void	printK(float kelvin){
+	printf("OUTPUT\nKELVIN: %.2f\n", kelvin);
+}
---
+++ FdP/CAA1-20241/retardified.c
@@ -0,0 +1,44 @@
+/*
+ * Author: Ivan Miranda Moral
+ * Date: 03-10-2024
+ * Description: This program takes a Fahrenheit temperature value input
+ * and converts and outputs it to a Kelvin temperature value.
+ */
+
+
+
+
+/* -------------------------------*/
+
+/* System header files */
+#include <stdio.h>
+
+
+
+
+
+
+/* -------------------------------*/ 
+
+/* Symbolic constants */
+
+#define F_FREEZE_POINT 32
+#define FIVE_NINTHS 5/9
+#define K_FREEZE_POINT 273.15
+
+
+/* -------------------------------*/
+
+/* Main function */
+int main(int argc, char **argv){
+	float fahrenheit;
+	float celsius;
+	float kelvin;
+
+	printf("INPUT\nFAHRENHEIT DEGREES?\n");
+	scanf("%2f", &fahrenheit);
+	celsius = (fahrenheit - F_FREEZE_POINT) * FIVE_NINTHS;
+	kelvin = (fahrenheit - F_FREEZE_POINT) * FIVE_NINTHS + K_FREEZE_POINT;
+	printf("OUTPUT\nKELVIN: %.2f\n", kelvin);
+	return 0;
+}
